a578f90f4ccc360198eca936376a9c1691ed09be
Nikhil Sai <nikhilsaigorantla2094@gmail.com>
2024-05-11 01:57:12 +0530

    added validation data

--- create_dataset.py
+++ create_dataset.py
@@ -8,15 +8,19 @@ random.seed(12345)
 os.system("rm -rf Datasets")
 os.system("mkdir Datasets")
 os.system("mkdir Datasets/train_dataset")
+os.system("mkdir Datasets/validation_dataset")
 os.system("mkdir Datasets/test_dataset")
 
 num_celebs = 200
 num_imgs = 30
-test_ratio = 0.2
-split_idx = int(num_imgs * (1-test_ratio))
+validation_ratio = 0.1
+test_ratio = 0.1
+split_idx_1 = int(num_imgs * (1 - validation_ratio - test_ratio))
+split_idx_2 = int(num_imgs * (1 - test_ratio))
 
 for i in range(num_celebs):
     os.system(f"mkdir Datasets/train_dataset/celeb_{i}")
+    os.system(f"mkdir Datasets/validation_dataset/celeb_{i}")
     os.system(f"mkdir Datasets/test_dataset/celeb_{i}")
 
 d = defaultdict(list)
@@ -32,8 +36,10 @@ with open("identity_CelebA.txt") as f:
     for i, celeb in enumerate(tqdm.tqdm(k[:num_celebs])):
         imgs = d[celeb]
         random.shuffle(imgs)
-        train_imgs, test_imgs = imgs[:split_idx], imgs[split_idx:]
+        train_imgs, validation_imgs, test_imgs = imgs[:split_idx_1], imgs[split_idx_1: split_idx_2], imgs[split_idx_2:]
         for img in train_imgs:
             os.system(f"cp img_align_celeba/{img} Datasets/train_dataset/celeb_{i}/{img}")
+        for img in validation_imgs:
+            os.system(f"cp img_align_celeba/{img} Datasets/validation_dataset/celeb_{i}/{img}")
         for img in test_imgs:
             os.system(f"cp img_align_celeba/{img} Datasets/test_dataset/celeb_{i}/{img}")
